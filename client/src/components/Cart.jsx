b3b7df48dda32c87170513e063372741ed3bf9b0
Hamza Amentag <hamentag1@gmail.com>
2024-03-26 15:25:20 -0500

    updated: display a list of all cart items

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -2,12 +2,38 @@ import { useState, useEffect } from "react";
 
 import { useParams, Link, useNavigate } from "react-router-dom";
 
-export default function Cart({cart}){
-   
+export default function Cart({auth, cart,products, setMsg, addToCart, removeFromCart}){
+    const { id } = useParams();
+
+
 
     return(
     <>
-       Cart
+        {
+            cart && 
+            <ul>
+        {
+            cart.map(item => {
+                const myProduct = products.find(product => product.id === item.product_id);
+                
+                return <li key={item.id}>
+                    <h4>{myProduct.title}</h4>  
+                    <div>Qty: {item.qty}</div>
+                    <div>
+                        <p>{myProduct.characteristics}.</p>
+                        <div>{myProduct.dimensions}</div>
+                        <div>{myProduct.inventory > 0? <p>In Stock</p>: <p>Out of Stock</p>}</div>
+                        <div>Inventory: {myProduct.inventory}</div>
+                       
+                       
+                    </div>
+                    <div></div>
+                    
+                </li>                                       
+            })
+        }
+        </ul>
+        }
     </>
     )
 }
